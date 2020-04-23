bacd9cf764ed988ae2196432a159fc8f3dad7e72
rafliaryansyah <raflyaryansha86@gmail.com>
2020-04-23 17:18:06 +0700

    update view register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,96 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
+
+<main class="login-container">
+    <div class="container">
+        <div class="row page-login d-flex align items-center">
+            <div class="section-right col-12 col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img src="{{ url('frontend/assets/images/logo.png') }}" alt="" class="w-50 mb-4">
+                        </div>
+                        <form action="{{ route('register') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+
+                                <label for="email">Fullname</label>
+
+                                <input type="name" name="name" class="form-control @error('name') is-invalid @enderror" id="" aria-describedby="" required autocomplete="email" autofocus value="">
+                                
+                                @error('name')
+                                <span class="invalid-feedback">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+
+                            </div>
+
+                            <div class="form-group">
+
+                                <label for="email">Username</label>
+
+                                <input type="text" class="form-control @error('email') is-invalid @enderror" id="" name="username" aria-describedby="" required autocomplete="email" autofocus>
+
+                                @error('username')
+                                <span class="invalid-feedback">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+
+                            </div>
+
+                            <div class="form-group">
+
+                                <label for="email">Email Address</label>
+
+                                <input type="email" class="form-control @error('email') is-invalid @enderror" id="" name="email" aria-describedby="" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                <span class="invalid-feedback">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+
+                            </div>
+
+                            <div class="form-group">
+
+                                <label for="password">Password</label>
+
+                                <input type="password" class="form-control @error('password') is-invalid @enderror" id="" name="password" name="" required autocomplete="current-password">
+
+                                @error('password')
+                                <span class="invalid-feedback">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+
+                            </div>
+
+                            <div class="form-group">
+
+                                <label for="password">Confirm Password</label>
+
+                                <input type="password" class="form-control" id="" name="password_confirmation" required autocomplete="current-password">
+
+                            </div>
+
+                            <button class="btn btn-login btn-block" type="submit">
+                                Register
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+@endsection
+
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -87,5 +176,4 @@
             </div>
         </div>
     </div>
-</div>
-@endsection
+</div> --}}
