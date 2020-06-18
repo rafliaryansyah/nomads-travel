7618d1063f08515a9c2736a405943ffb3ba97d92
rafliaryansyah <raflyaryansha86@gmail.com>
2020-06-18 08:37:53 +0700

    delete category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -98,6 +98,9 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $category = Category::findOrFail($id);
+        $category->delete();
+
+        return redirect()->route('category.index');
     }
 }
